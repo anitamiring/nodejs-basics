7f1ea1ebd2a739596a9543879a8f0ef49bd54e44
Swizzle <miringanita@gmail.com>
2020-03-22 08:55:13 +0100

    Parsing request body

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (req, res) => 
 {
     console.log(req.url, req.method, req.headers);
     //process.exit();
+
+    const url = req.url;
+    const method = req.method;
+
+    if( url === '/' )
+    {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if( url === '/message' && method === 'POST')
+    {
+        const body = [];
+        req.on('data', (chunk) => 
+        {
+            console.log(chunk)
+            body.push(chunk);
+        });
+        req.on('end', () => 
+        {
+            const parsedBody = Buffer.concat(body).toString();
+            //parsedBody == 'message=Anita+Miring'
+
+            let message = parsedBody.split('=')[1];
+            //message == 'Anita+Miring'
+
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302; //redirection
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
